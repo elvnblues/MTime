9c3225569b7af1fda3674af16fe1306940a5183e
elvnblues <elvnblues@gmail.com>
2013-06-24 16:53:05 +0800

    update README.md

--- MTimer/src/com/xuan/mtimer/MTimeServer.java
+++ MTimer/src/com/xuan/mtimer/MTimeServer.java
@@ -1,10 +1,13 @@
 package com.xuan.mtimer;
 
+import java.util.Timer;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
 public class MTimeServer extends Service {
+	private Timer timer1;
 
 	@Override
 	public IBinder onBind(Intent intent) {
